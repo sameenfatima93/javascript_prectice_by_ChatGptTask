a21f6b100b3b47a4defa0ad197774d98cd28ec67
SameenFatima <sameen.fatima993@gmail.com>
2025-11-01 13:07:30 +0500

    taskONE loop inside function

---
+++ Tast1(usinglooop_insideTheFunction)/script.js
@@ -0,0 +1,48 @@
+////////________prectice loop_______///////////////
+
+// var name = prompt("Enter any word to check if it's a palindrome or not");
+// var result = "";
+
+// for (var i = name.length - 1; i >= 0; i--) {
+//   result += name[i];
+// }
+
+// if (name.toLowerCase() === result.toLowerCase()) {
+//   console.log("It's a palindrome");
+// } else {
+//   console.log("It's not a palindrome");
+// }
+
+// console.log(result);
+
+
+//////////////////////////////////////////
+///__________________________////////////
+////  MOVING TO THE MAIN TAS   ////////////
+///___________________________////////
+// ////////////////////////////////////////
+
+// function findPalindrome(str){
+//     var result = "";
+//     for(var i = str.length -1 ; i >= 0 ; i-- ){
+//         result += str[i];
+//     }
+//     if(str.toLowerCase() === result.toLowerCase()){
+//         console.log("it's a Palandirome");
+        
+//     }else{
+//         console.log("it's Not a palandirom");
+        
+//     }
+//     // return result;
+// }
+// findPalindrome("madam")
+// findPalindrome("sameen")
+// console.log("");
+
+// console.log(findPalindrome("hello"));
+
+function greet(name){
+    return name;
+}
+console.log(greet("hello sameen"));
