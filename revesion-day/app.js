adeaf5541b74bf86207a020d4eb4c53e3656fc2d
SameenFatima <sameen.fatima993@gmail.com>
2025-12-02 10:44:33 +0500

    revision

---
+++ revesion-day/app.js
@@ -0,0 +1,22 @@
+//____revision arry and loop______________//
+
+let arr = ["apple","mango","banana","orange","strowbery","graphs","pinpple"];
+for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+    
+    
+}
+//arr.pop() // remove an item fron last
+// arr.push("drigenfruit") // incert an item in the last of arry
+//arr.shift() // remove item from start of arry
+//arr.unshift("peanut") // incert iteme in the start of arry 
+// arr.filter(item => {
+//     console.log(item);
+    
+// }) // ye koch return nhi krna just show krwata hy arry ky tammam item and un pr callback function chala dyta hy
+// let newArr = arr.map(function (fruits){
+//     return fruits.toUpperCase()
+// }) //not 100% clear................
+// console.log(newArr); //this is a return a new arry 
+
+// console.log(arr);
